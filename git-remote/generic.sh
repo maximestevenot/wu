dd1f32650f8874e7aa5b384b7d76c8fa2105aec8
Maxime STEVENOT <maxime.stevenot@manomano.com>
2024-08-21 09:26:17 +0200

    feat: improve gcr fct to auto suffix with .git

--- git-remote/generic.sh
+++ git-remote/generic.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 function git-remote-to-local() {
-    local extracted_path="$(echo "$1" | sed 's#https://[^/]*/##; s#\.git$##')"
+    local extracted_path
+    extracted_path="$(echo "$1" | sed 's#https://[^/]*/##; s#\.git$##')"
     echo "${SOURCES_FOLDER}/${extracted_path}"
 }
 
@@ -10,10 +11,13 @@ function cr() {
 }
 
 function gcr() {
+    local repo_url="$1"
     local local_repo
-    local_repo=$(git-remote-to-local "$1")
-
+    if [[ ! "$repo_url" =~ \.git$ ]]; then
+        repo_url="${repo_url}.git"
+    fi
+    local_repo=$(git-remote-to-local "$repo_url")
     mkdir -p "$local_repo"
-    git clone "$1" "$local_repo"
-    cr "$1"
+    git clone "$repo_url" "$local_repo"
+    cd "$local_repo" || exit
 }
